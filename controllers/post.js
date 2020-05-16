9a975749d2f921b3e6b701c3fe4f4b8369097a80
Yinka Robert <yinkakinbolajo@gmail.com>
2020-05-16 04:28:31 +0300

    NodeApi-Blog-Code Clean Up

--- controllers/post.js
+++ controllers/post.js
@@ -9,14 +9,6 @@ exports.getPosts = (req, res) => {
 
 exports.createPost = (req, res) => {
     const post = new Post(req.body);
-    // console.log("Created Post: ", req.body);
-    // post.save((result) => {
-    //     res.status(200).json({
-    //        post: result
-    //     });
-    //
-    // });
-
     post.save().then(result => {
         res.status(200).json({
            post: result
